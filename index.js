6be42b268cb75affa93556636ba808f8ca901686
ParkJeongHo <pjh31208@naver.com>
2022-02-12 01:26:52 +0900

    비밀정보 보호

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser');
+const {User} = require("./models/User");
+const config = require("./config/key");
+
+// 바디펄서가 클라이언트에서 오는 정보를 서버에서 분석해서 가져올 수 있게 해준다.
+// application/ x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://Park:wjdghqkr03!@cluster0.jvxiu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',  { 
+mongoose.connect(config.mongoURI,  { 
   useNewUrlParser: true, 
   useUnifiedTopology: true, 
   // useCreateIndex: true, 
@@ -12,7 +21,21 @@ mongoose.connect('mongodb+srv://Park:wjdghqkr03!@cluster0.jvxiu.mongodb.net/myFi
   .catch(e => console.error(e));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!!!!!!')
+  res.send('Hello World!')
+})
+
+
+app.post('/register', (req, res) => {
+
+  //회원 가입 할때 필요한 정보들을 클라이언트에서 가져오면 그것들을 데이터베이스에 넣어준다.
+  const user = new User(req.body)
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({ success: false, err })
+    return res.status(200).json({
+      success: true
+    })
+  })
 })
 
 app.listen(port, () => {
